d44c472121674685be29e4cb3ac522d2ea6183fb
Emonch <E-monch@outlook.com>
2019-03-13 23:12:59 +0800

    点对点测试完成

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,51 @@
 package com.example.demo;
 
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTempTopic;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+import javax.jms.Topic;
 
 @SpringBootApplication
 public class DemoApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public Queue getQueue()
+    {
+        return new ActiveMQQueue("queue");
+    }
+
+    @Bean
+    public Topic getTopic()
+    {
+        return new ActiveMQTopic("topic");
+    }
+
+    /*// topic模式的ListenerContainer
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerTopic(ConnectionFactory activeMQConnectionFactory) {
+        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+        bean.setPubSubDomain(true);
+        bean.setConnectionFactory(activeMQConnectionFactory);
+        return bean;
+    }
+    // queue模式的ListenerContainer
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerQueue(ConnectionFactory activeMQConnectionFactory) {
+        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+        bean.setConnectionFactory(activeMQConnectionFactory);
+        return bean;
+    }*/
 }
